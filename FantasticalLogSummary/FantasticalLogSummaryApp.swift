129181f19281b43d7a9438bf116b03c453f6a5d4
Haddock <consti.loew@live.com>
2022-11-28 08:05:19 +0100

    Initial Commit

---
+++ FantasticalLogSummary/FantasticalLogSummaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FantasticalLogSummaryApp.swift
+//  FantasticalLogSummary
+//
+//  Created by Constantin Loew on 28.11.22.
+//
+
+import SwiftUI
+
+@main
+struct FantasticalLogSummaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
