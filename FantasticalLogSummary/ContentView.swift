ba8ac2c93f2f567e61c66a7de118369eb77a2c18
Haddock <consti.loew@live.com>
2022-11-29 10:22:00 +0100

    Make zip files parse faster and more robust

--- FantasticalLogSummary/ContentView.swift
+++ FantasticalLogSummary/ContentView.swift
@@ -124,25 +124,33 @@ struct ContentView: View {
     func openLogFile(_ url: URL) {
         self.filename = url.lastPathComponent
         do {
-            var logString = ""
             if url.pathExtension == "zip" {
                 // file is zip, unpack and concat
                 let file_name = url.lastPathComponent.prefix(url.lastPathComponent.count - 4)
                 let destDir = FileManager.default.temporaryDirectory.appending(path: file_name)
-                print(destDir.path)
                 try Zip.unzipFile(url, destination: destDir, overwrite: true, password: nil)
-                let dirEnumerator = FileManager.default.enumerator(at: destDir, includingPropertiesForKeys: [.isDirectoryKey])!
-                for case let file as URL in dirEnumerator {
-                    if file.pathExtension == "log" && !file.path().contains("helper") {
-                        let fileString = try String(contentsOf: file)
-                        logString += fileString
+                
+                // try enumerating over all unzipped files
+                if let dirEnumerator = FileManager.default.enumerator(at: destDir, includingPropertiesForKeys: [.isDirectoryKey]) {
+                    calendarStores = []
+                    for case let file as URL in dirEnumerator {
+                        if file.pathExtension == "log" && !file.path().contains("helper") {
+                            let fileString = try String(contentsOf: file)
+                            calendarStores += parseCalendarStores(fileString)
+                        }
                     }
+                } else {
+                    failedParsing = true
+                    calendarStores = []
+                    print("Error: failed creating enumerator on \(destDir.path)")
                 }
+                
+                
             } else {
                 // treat file as a log directly
-                logString = try String(contentsOf: url)
+                let logString = try String(contentsOf: url)
+                calendarStores = parseCalendarStores(logString)
             }
-            calendarStores = parseCalendarStores(logString)
             failedParsing = false
         } catch let error {
             failedParsing = true
